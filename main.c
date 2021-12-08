09e7f4d74574b7fbc441441f263cff8bc6ec30c2
Emile SIVRE <esivre@e3r7p17.clusters.42paris.fr>
2021-12-09 00:01:14 +0100

    lolilol

--- main.c
+++ main.c
@@ -3,10 +3,21 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 
+typedef struct s_offset
+{
+	int x_min;
+	int y_min;
+	int x_max;
+	int y_max;
+}	t_offset;
+
 typedef struct s_point
 {
-	int x;
-	int y;
+	float x;
+	float y;
+	int x3d;
+	int y3d;
+	int z3d;
 }	t_point;
 
 typedef struct s_map
@@ -16,6 +27,7 @@ typedef struct s_map
 	int scale;
 	int offset_x;
 	int offset_y;
+	t_offset offset;
 	t_point pt1;
 	t_point pt2;
 }	t_map;
@@ -69,6 +81,72 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+void	plotLineLow(t_data *data, t_point pt0, t_point pt1)
+{
+	int D;
+	int x;
+	int y;
+
+ 	D = (2 * (abs((int)pt1.y - (int)pt0.y))) - (pt1.x - pt0.x);
+    y = pt0.y;
+	x = pt0.x;
+	while (x < pt1.x)
+	{
+   		my_mlx_pixel_put(data, x, y, 0x00FFF000);
+   	   	if (D > 0)
+		{
+			if (pt1.y > pt0.y)
+   	      	 	y++;
+			else
+				y--;
+   	       	D = D + (2 * (abs((int)pt1.y - (int)pt0.y)) - (pt1.x - pt0.x));
+		}
+   	   	else
+   	       	D = D + 2 * abs((int)pt1.y - (int)pt0.y);
+		x++;
+	}
+}
+
+void	plotLineHigh(t_data *data, t_point pt0, t_point pt1)
+{
+	int D;
+	int x;
+	int y;
+
+    D = (2 * abs((int)pt1.x - (int)pt0.x)) - (pt1.y - pt0.y);
+    x = pt0.x;
+	y = pt0.y;
+	while (y < pt1.y)
+	{
+        my_mlx_pixel_put(data, x, y, 0x00FFF000);
+        if (D > 0)
+		{
+            if (pt1.x > pt0.x)
+   	      	 	x++;
+			else
+				x--;
+            D = D + (2 * (abs((int)pt1.x - (int)pt0.x) - (pt1.y - pt0.y)));
+		}
+        else
+            D = D + 2*abs((int)pt1.x - (int)pt0.x);
+		y++;
+	}
+}
+
+void drawline(t_data *data, t_point pt0, t_point pt1)
+{
+    if (abs((int)pt1.y - (int)pt0.y) < abs((int)pt1.x - (int)pt0.x))
+        if (pt0.x > pt1.x)
+            plotLineLow(data, pt1, pt0);
+        else
+            plotLineLow(data, pt0, pt1);
+    else
+        if (pt0.y > pt1.y)
+            plotLineHigh(data, pt1, pt0);
+        else
+            plotLineHigh(data, pt0, pt1);
+}
+
 void	draw_line(t_data *data, t_point pt1, t_point pt2)
 {
 	float a;
@@ -102,7 +180,7 @@ void map_init(t_map *map)
 	map->y_size = 0;
 }
 
-int x_proj(t_map map, int x, int y)
+float x_proj(t_map map, int x, int y)
 {
 	float ret;
 
@@ -110,11 +188,11 @@ int x_proj(t_map map, int x, int y)
 	return (map.scale * ret);
 }
 
-int y_proj(t_map map, int x, int y, int z)
+float y_proj(t_map map, int x, int y, int z)
 {
 	float ret;
 
-	ret = -1.4142/1.7321 * (float)z + 1/2.4495 * ((float)x + (float)y);
+	ret = 1.4142/1.7321 * (float)y + 1/2.4495 * ((float)x - (float)z);
 	return (map.scale * ret);
 }
 
@@ -124,7 +202,39 @@ t_point	set_point(t_map map, int **tab, int i, int j)
 
 	pnt.x = map.offset_x + x_proj(map, i, j);
 	pnt.y = map.offset_y + y_proj(map, i, j, tab[i][j]); 
+	pnt.x3d = i;
+	pnt.y3d = j;
+	pnt.z3d = tab[i][j];
 	return (pnt);
+}
+
+t_offset	shift_offset(int **tab, t_map map)
+{
+	t_offset offset;
+	int i;
+
+	map.pt1 = set_point(map, tab, map.x_size - 1, map.y_size - 1);
+	offset.x_min = map.pt1.x;
+	offset.y_min = map.pt1.y;
+	offset.x_max = map.pt1.x;
+	offset.y_max = map.pt1.y;
+	while (map.y_size-- > 0)
+	{	
+		i = 0;
+		while (i < map.x_size)
+		{
+			map.pt1 = set_point(map, tab, i++, map.y_size);
+			if (map.pt1.x < offset.x_min)
+				offset.x_min = map.pt1.x;
+			if (map.pt1.y < offset.y_min)
+				offset.y_min = map.pt1.y;
+			if (map.pt1.x > offset.x_max)
+				offset.x_max = map.pt1.x;
+			if (map.pt1.y > offset.y_max)
+				offset.y_max = map.pt1.y;
+		}
+	}
+	return (offset);
 }
 
 void	draw_map(int **tab, t_map map, t_data *img)
@@ -140,18 +250,18 @@ void	draw_map(int **tab, t_map map, t_data *img)
 		{
 			map.pt1 = set_point(map, tab, i, j);
 			map.pt2 = set_point(map, tab, i + 1, j);
-			draw_line(img, map.pt1, map.pt2);
+			drawline(img, map.pt1, map.pt2);
 			if (j + 1 < map.y_size)
 			{
 				map.pt2 = set_point(map, tab, i, j + 1);
-				draw_line(img, map.pt1, map.pt2);
+				drawline(img, map.pt1, map.pt2);
 			}
 		}
 		if (j + 1 < map.y_size)
 		{
 			map.pt1 = set_point(map, tab, i, j);
 			map.pt2 = set_point(map, tab, i, j + 1);
-			draw_line(img, map.pt1, map.pt2);	
+			drawline(img, map.pt1, map.pt2);	
 		}
 	}
 }
@@ -182,16 +292,22 @@ void	free_doublechar(char **str)
 int check_line(int fd, char **line, t_map *map)
 {
 	char **parse_line;
+	int i;
 
 	parse_line = ft_split(*line, ' ');
 	if (!parse_line)
 		quit(1, 0, 0);
 	free(*line);
 	*line = NULL;
-	while (parse_line[map->x_size] && !(map->y_size))
-		(map->x_size)++;
+	i = 0;
+	while (parse_line[i])
+		i++;
 	free_doublechar(parse_line);
 	parse_line = NULL;
+	if (!map->x_size)
+			map->x_size = i;
+	else if (map->x_size != i)
+			quit(1, 0, 0);
 	(map->y_size)++;
 	return (get_next_line(fd, line));
 }
@@ -203,6 +319,8 @@ void check_map(t_map *map, char **argv)
 	int gnl;
 	char *line;
 
+	map->x_size = 0;
+	map->y_size = 0;
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		quit(1, 0, 0);
@@ -212,6 +330,8 @@ void check_map(t_map *map, char **argv)
 	while(gnl > 0)
 		gnl = check_line(fd, &line, map);
 	free(line);
+	printf("size y %d\n", map->y_size);
+	printf("size x %d\n", map->x_size);
 	if (close(fd) || gnl < 0)
 		quit(1, 0, 0);
 }
@@ -285,27 +405,42 @@ int main(int argc, char **argv)
 	t_data	img;
 	int	**tab;
 	t_map map;
+	t_offset off;
 
 	if (argc != 2)
 		return (0);
 	check_map(&map, argv);
-	map.offset_x = 500;
-	map.offset_y = 500;
-	map.scale = 20;
+	map.offset_x = 1920 / 2;
+	map.offset_y = 1080 / 2;
+	map.scale = 1;
 	tab = alloc_tab(map.x_size, map.y_size);
 	fill_tab(tab, map, argv);
 	mlx = mlx_init();
+	if (!mlx)
+		return (0);
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "FdF");
+	if (!mlx_win)
+		return (0);
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 //	my_mlx_pixel_put(&img, 5, 5, 0x00FFFF00);
+	off = shift_offset(tab, map);
+	map.scale = min(1920 / (off.x_max-off.x_min), 1080 / (off.y_max-off.y_min)) * 0.8;
+	if (map.scale < 1)
+		map.scale = 1;
+	off = shift_offset(tab, map);
+	map.offset_x = 2 * map.offset_x - (off.x_max + off.x_min)/2;
+	map.offset_y = 2 * map.offset_y - (off.y_max + off.y_min)/2;
+	off = shift_offset(tab, map);
 	draw_map(tab, map, &img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+
+//	mlx_hook(mlx_win, 2, 0, &key_event_press, &e);
+//	mlx_key_hook(mlx_win, &key_event_take_off, &e);
+//	mlx_loop_hook(mlx, &alteration, &e);
 	mlx_loop(mlx);
 	free_tab(tab, map);
-	
-
-
+	free(mlx);
 	return (0);
 }
